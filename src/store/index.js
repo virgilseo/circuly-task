d1cc7927a2af920630b06413c79d52550de05df0
Virgil-Avram <avraam.virgil@yahoo.com>
2020-04-05 19:49:18 +0200

    Store client datababe in global state

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,17 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+// Import customer datababe
+
+import clientData from "@/json/test.json";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: clientData,
   mutations: {},
   actions: {},
   modules: {}
 });
+
+console.log(clientData);
