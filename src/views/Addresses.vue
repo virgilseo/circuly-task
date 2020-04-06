b6216a969c62b595823152e783a84621822cd40a
Virgil-Avram <avraam.virgil@yahoo.com>
2020-04-06 10:42:49 +0200

    Display all available address information on the page

--- src/views/Addresses.vue
+++ src/views/Addresses.vue
@@ -4,19 +4,11 @@
     <ul>
       <!-- Loop trought client addresses and render them on the page -->
       <li v-for="address in addresses" :key="address.id" >
-        <span>countryCodeAlpha2: {{address.countryCodeAlpha2}}</span>
-        <span>countryCodeAlpha3: {{address.countryCodeAlpha3}}</span>
-        <span>countryCodeNumeric: {{address.countryCodeNumeric}}</span>
-        <span>countryName: {{address.countryName}}</span>
-        <span>createdAt: date: {{address.createdAt.date}} timezone: {{address.createdAt.timezone}} timezone_type: {{address.createdAt.timezone_type}}</span>
-        <span>customerId: {{address.customerId}}</span>
-        <span>firstName: {{address.firstName}}</span>
-        <span>lastName: {{address.lastName}}</span>
-        <span>id: {{address.id}}</span>
-        <span>locality: {{address.locality}}</span>
-        <span>postalCode: {{address.postalCode}}</span>
-        <span>streetAddress: {{address.streetAddress}}</span>
-        <span>updatedAt: date: {{address.updatedAt.date}} timezone: {{address.updatedAt.timezone}} timezone_type: {{address.updatedAt.timezone_type}} </span>
+        <ul>
+          <li v-for="(value, propertyName, index) in address" :key="index">
+            {{propertyName}}: {{value}}
+          </li>
+        </ul>
       </li>
     </ul>
   </div>
