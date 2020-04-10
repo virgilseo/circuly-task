6a3f600963f67ce3801b7385bb031a4d3ccdbd50
Virgil-Avram <avraam.virgil@yahoo.com>
2020-04-10 10:51:53 +0200

    Fetch new address from the server

--- src/views/NewAddress.vue
+++ src/views/NewAddress.vue
@@ -8,3 +8,40 @@
     </ul>
   </div>
 </template>
+
+<script>
+
+export default {
+
+// Set the initial state of the component
+
+  data() {
+
+    return {
+      hits: null,
+      loading: true,
+      error: false
+    }
+  },
+
+// Fetch new address from the server after the component has mounted
+
+  mounted() {
+
+    const axios = require('axios');
+
+    axios.get('http://localhost:3000/newAddress')
+    .then(response => {
+      this.hits = response.data
+      console.log(this.hits)
+    })
+    .catch(error => {
+      console.log(error)
+      this.error = true
+    })
+    .finally(() => this.loading = false)
+  }
+}
+
+
+</script>
