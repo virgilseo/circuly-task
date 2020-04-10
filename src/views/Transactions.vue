d059c340cba2394e2940fd020e2c0318eefce386
Virgil-Avram <avraam.virgil@yahoo.com>
2020-04-10 15:06:45 +0200

    Display only relevant info on the transactions page

--- src/views/Transactions.vue
+++ src/views/Transactions.vue
@@ -5,9 +5,18 @@
       <!-- Loop trought client transactions and render them on the page -->
       <li v-for="transaction in transactions" :key="transaction.id">
         <ul>
-          <li v-for="(value, propertyName, index) in transaction" :key="index">
-            {{propertyName}}: {{value}}
-          </li>
+          <li><span>Amount: </span><span>{{transaction.amount}}</span></li>
+          <li><span>Currency: </span><span>{{transaction.currencyIsoCode}}</span></li>
+          <li><span>Merchant account id: </span><span>{{transaction.merchantAccountId}}</span></li>
+          <li><span>Payment type: </span><span>{{transaction.paymentInstrumentType}}</span></li>
+          <li><span>Plan id: </span><span>{{transaction.planId}}</span></li>
+          <li><span>Authorization code: </span><span>{{transaction.processorAuthorizationCode}}</span></li>
+          <li><span>Settlement batch id: </span><span>{{transaction.settlementBatchId}}</span></li>
+          <li><span>Status: </span><span>{{transaction.status}}</span></li>
+          <li><span>Subscription id: </span><span>{{transaction.subscriptionId}}</span></li>
+          <li><span>Type: </span><span>{{transaction.type}}</span></li>
+          <li><span>Created: </span><span>{{transaction.createdAt.date}} {{transaction.createdAt.timezone}}</span></li>
+          <li><span>Updated: </span><span>{{transaction.updatedAt.date}} {{transaction.updatedAt.timezone}}</span></li>
         </ul>
       </li>
     </ul>
