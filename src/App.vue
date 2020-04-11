9204917ac01924c9b37b10842e888024456573f3
Virgil-Avram <avraam.virgil@yahoo.com>
2020-04-11 10:44:38 +0200

    Update navigation style

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to='/transactions'>Transactions</router-link> |
-      <router-link to='/addresses'>Addresses</router-link> |
-      <router-link to='/add'>Add address</router-link> |
+      <router-link to="/">Home</router-link>
+      <router-link to='/transactions'>Transactions</router-link>
+      <router-link to='/addresses'>Addresses</router-link>
+      <router-link to='/add'>Add address</router-link> 
       <router-link to='/new-address'>New address</router-link>
     </div>
   <router-view />
