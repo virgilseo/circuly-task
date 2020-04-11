ab97275704fa4a7c092cc2a80d81b3db0b23030b
Virgil-Avram <avraam.virgil@yahoo.com>
2020-04-11 12:01:05 +0200

    Add style to transactions page

--- src/views/Transactions.vue
+++ src/views/Transactions.vue
@@ -1,23 +1,23 @@
 <template>
-  <div class="transactions">
-    <h1>Transactions</h1>
+  <div class="transactions-container">
+    <h1 class="page-titles">Transactions</h1>
     <ul>
       <!-- Loop trought client transactions and render them on the page -->
-      <li v-for="transaction in transactions" :key="transaction.id">
-        <ul>
-          <li><span>Client name: </span><span>{{transaction.billing.firstName}} {{transaction.billing.lastName}}</span></li>
-          <li><span>Amount: </span><span>{{transaction.amount}}</span></li>
-          <li><span>Currency: </span><span>{{transaction.currencyIsoCode}}</span></li>
-          <li><span>Merchant account id: </span><span>{{transaction.merchantAccountId}}</span></li>
-          <li><span>Payment type: </span><span>{{transaction.paymentInstrumentType}}</span></li>
-          <li><span>Plan id: </span><span>{{transaction.planId}}</span></li>
-          <li><span>Authorization code: </span><span>{{transaction.processorAuthorizationCode}}</span></li>
-          <li><span>Settlement batch id: </span><span>{{transaction.settlementBatchId}}</span></li>
-          <li><span>Status: </span><span>{{transaction.status}}</span></li>
-          <li><span>Subscription id: </span><span>{{transaction.subscriptionId}}</span></li>
-          <li><span>Type: </span><span>{{transaction.type}}</span></li>
-          <li><span>Created: </span><span>{{transaction.createdAt.date}} {{transaction.createdAt.timezone}}</span></li>
-          <li><span>Updated: </span><span>{{transaction.updatedAt.date}} {{transaction.updatedAt.timezone}}</span></li>
+      <li class="transaction-container" v-for="transaction in transactions" :key="transaction.id">
+        <ul class="list-container">
+          <li><span class="transaction-label">Client name </span><span class="transaction-item">{{transaction.billing.firstName}} {{transaction.billing.lastName}}</span></li>
+          <li><span class="transaction-label">Amount </span><span class="transaction-item">{{transaction.amount}}</span></li>
+          <li><span class="transaction-label">Currency </span><span class="transaction-item">{{transaction.currencyIsoCode}}</span></li>
+          <li><span class="transaction-label">Merchant account id </span><span class="transaction-item">{{transaction.merchantAccountId}}</span></li>
+          <li><span class="transaction-label">Payment type </span><span class="transaction-item">{{transaction.paymentInstrumentType}}</span></li>
+          <li><span class="transaction-label">Plan id </span><span class="transaction-item">{{transaction.planId}}</span></li>
+          <li><span class="transaction-label">Authorization code </span><span class="transaction-item">{{transaction.processorAuthorizationCode}}</span></li>
+          <li><span class="transaction-label">Settlement batch id </span><span class="transaction-item">{{transaction.settlementBatchId}}</span></li>
+          <li><span class="transaction-label">Status </span><span class="transaction-item">{{transaction.status}}</span></li>
+          <li><span class="transaction-label">Subscription id </span><span class="transaction-item">{{transaction.subscriptionId}}</span></li>
+          <li><span class="transaction-label">Type </span><span class="transaction-item">{{transaction.type}}</span></li>
+          <li><span class="transaction-label">Created </span><span class="transaction-item">{{transaction.createdAt.date}} {{transaction.createdAt.timezone}}</span></li>
+          <li><span class="transaction-label">Updated </span><span class="transaction-item">{{transaction.updatedAt.date}} {{transaction.updatedAt.timezone}}</span></li>
         </ul>
       </li>
     </ul>
