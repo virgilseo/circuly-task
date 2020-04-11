6ef8387c1efe95052b66677c3fba477429631f9e
Virgil-Avram <avraam.virgil@yahoo.com>
2020-04-11 10:16:44 +0200

    Add main css style sheet

--- src/App.vue
+++ src/App.vue
@@ -12,24 +12,7 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
+@import './assets/stylesheets/app.css';
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
