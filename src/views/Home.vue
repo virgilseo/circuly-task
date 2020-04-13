cebfa23b1610d4fea107e26e466bb32dad789de9
Virgil-Avram <avraam.virgil@yahoo.com>
2020-04-13 11:54:33 +0200

    Add home page message

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,10 @@
 <template>
   <div class="home">
+    <div class="message-container">
+      <p>Hello <span class="circuly-text">Circuly</span>. I just wanted to say thanks for considering me for
+        the front end developer possition.
+      </p>
+      <p>I wish you all the <span class="best-text">best</span>.</p>
+    </div>
   </div>
 </template>
